10c7b35c53ecf6e42f1e962b362bbfe482e76852
Sam Woodhead <samwoodhead@sudlows.com>
2018-09-19 10:40:08 +0100

    Setup source to highlight minimum reproducible error

--- DockableDialog/MainPage.xaml.cs
+++ DockableDialog/MainPage.xaml.cs
@@ -1,43 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Net;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using Microsoft.Win32;
-using System.Windows.Navigation;
-using System.Windows.Controls.Primitives;
-using System.Reflection;
-using System.Drawing;
-using System.Configuration;
-using System.Collections.Generic;
 using Autodesk.Revit.UI;
-using System.Text;
 
 namespace DockableDialog.Forms
 {
-  /// <summary>
-  /// Interaction logic for UserControl1.xaml
-  /// </summary>
   public partial class MainPage : Page, Autodesk.Revit.UI.IDockablePaneProvider
   {
-    #region Data
-    private Guid m_targetGuid;
-    private DockPosition m_position = DockPosition.Bottom;
-    private int m_left = 1;
-    private int m_right = 1;
-    private int m_top = 1;
-    private int m_bottom = 1;
-    const string _url_tbc = "http://thebuildingcoder.typepad.com";
-    const string _url_git = "https://github.com/jeremytammik/DockableDialog";
-    #endregion
-
     public MainPage()
     {
       InitializeComponent();
@@ -48,103 +17,7 @@ namespace DockableDialog.Forms
       data.FrameworkElement = this as FrameworkElement;
       data.InitialState = new Autodesk.Revit.UI.DockablePaneState();
       data.InitialState.DockPosition = DockPosition.Tabbed;
-      //DockablePaneId targetPane;
-      //if (m_targetGuid == Guid.Empty)
-      //    targetPane = null;
-      //else targetPane = new DockablePaneId(m_targetGuid);
-      //if (m_position == DockPosition.Tabbed)
       data.InitialState.TabBehind = Autodesk.Revit.UI.DockablePanes.BuiltInDockablePanes.ProjectBrowser;
-      //if (m_position == DockPosition.Floating)
-      //{
-      //data.InitialState.SetFloatingRectangle(new Autodesk.Revit.UI.Rectangle(10, 710, 10, 710));
-      //data.InitialState.DockPosition = DockPosition.Tabbed;
-      //}
-      //Log.Message("***Intial docking parameters***");
-      //Log.Message(APIUtility.GetDockStateSummary(data.InitialState));
-    }
-
-    public void SetInitialDockingParameters( int left, int right, int top, int bottom, DockPosition position, Guid targetGuid )
-    {
-      m_position = position;
-      m_left = left;
-      m_right = right;
-      m_top = top;
-      m_bottom = bottom;
-      m_targetGuid = targetGuid;
-    }
-
-    private void PaneInfoButton_Click(
-      object sender,
-      RoutedEventArgs e )
-    {
-      web_browser.Navigate( _url_tbc );
-    }
-
-    private void wpf_stats_Click(
-      object sender,
-      RoutedEventArgs e )
-    {
-      web_browser.Navigate( _url_git );
-    }
-
-    private void btn_getById_Click(
-      object sender,
-      RoutedEventArgs e )
-    {
-      web_browser.Navigate( _url_tbc );
-    }
-
-    private void btn_listTabs_Click(
-      object sender,
-      RoutedEventArgs e )
-    {
-      web_browser.Navigate( _url_git );
-    }
-
-    private void DockableDialogs_Loaded(
-      object sender,
-      RoutedEventArgs e )
-    {
-      web_browser.Navigated += new NavigatedEventHandler(
-        WebBrowser_Navigated );
-
-      web_browser.Navigate( _url_tbc );
-    }
-
-    void WebBrowser_Navigated(
-      object sender,
-      NavigationEventArgs e )
-    {
-      HideJsScriptErrors( (WebBrowser) sender );
-    }
-
-    public void HideJsScriptErrors( WebBrowser wb )
-    {
-      // IWebBrowser2 interface
-      // Exposes methods that are implemented by the 
-      // WebBrowser control
-      // Searches for the specified field, using the 
-      // specified binding constraints.
-
-      FieldInfo fld = typeof( WebBrowser ).GetField(
-        "_axIWebBrowser2",
-        BindingFlags.Instance | BindingFlags.NonPublic );
-
-      if( null != fld )
-      {
-        object obj = fld.GetValue( wb );
-        if( null != obj )
-        {
-          // Silent: Sets or gets a value that indicates 
-          // whether the object can display dialog boxes.
-          // HRESULT IWebBrowser2::get_Silent(VARIANT_BOOL *pbSilent);
-          // HRESULT IWebBrowser2::put_Silent(VARIANT_BOOL bSilent);
-
-          obj.GetType().InvokeMember( "Silent",
-            BindingFlags.SetProperty, null, obj,
-            new object[] { true } );
-        }
-      }
     }
   }
 }
--- DockableDialog/Ribbon.cs
+++ DockableDialog/Ribbon.cs
@@ -3,185 +3,79 @@ using System.Reflection;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.UI.Events;
 using DockableDialog.Forms;
-using DockableDialog.Properties;
 
 namespace DockableDialog
 {
-  public class Ribbon : IExternalApplication
-  {
-    public Result OnStartup(
-      UIControlledApplication a )
+    public class Ribbon : IExternalApplication
     {
-      a.CreateRibbonTab( "AEC LABS" );
+        public Result OnStartup(UIControlledApplication a)
+        {
+            a.CreateRibbonTab("Example");
 
-      RibbonPanel AECPanelDebug
-        = a.CreateRibbonPanel( "AEC LABS", "AEC LABS" );
+            RibbonPanel AECPanelDebug = a.CreateRibbonPanel("Example", "AEC LABS");
 
-      string path = Assembly.GetExecutingAssembly().Location;
+            string path = Assembly.GetExecutingAssembly().Location;
 
-      #region DockableWindow
-      PushButtonData pushButtonRegisterDockableWindow = new PushButtonData( "RegisterDockableWindow", "RegisterDockableWindow", path, "DockableDialog.RegisterDockableWindow" );
-      pushButtonRegisterDockableWindow.LargeImage = GetImage( Resources.green.GetHbitmap() );
-      pushButtonRegisterDockableWindow.AvailabilityClassName = "DockableDialog.AvailabilityNoOpenDocument";
-      PushButtonData pushButtonShowDockableWindow = new PushButtonData( "Show DockableWindow", "Show DockableWindow", path, "DockableDialog.ShowDockableWindow" );
-      pushButtonShowDockableWindow.LargeImage = GetImage( Resources.red.GetHbitmap() );
-      PushButtonData pushButtonHideDockableWindow = new PushButtonData( "Hide DockableWindow", "Hide DockableWindow", path, "DockableDialog.HideDockableWindow" );
-      pushButtonHideDockableWindow.LargeImage = GetImage( Resources.orange.GetHbitmap() );
-      //IList<RibbonItem> ribbonpushButtonDockableWindow = AECPanelDebug.AddStackedItems(pushButtonRegisterDockableWindow, pushButtonShowDockableWindow, pushButtonHideDockableWindow);
+            #region DockableWindow
+            PushButtonData pushButtonRegisterDockableWindow = new PushButtonData("RegisterDockableWindow", "Register", path, "DockableDialog.RegisterDockableWindow");
+            pushButtonRegisterDockableWindow.AvailabilityClassName = "DockableDialog.AvailabilityNoOpenDocument";
+            RibbonItem ri1 = AECPanelDebug.AddItem(pushButtonRegisterDockableWindow);
+            #endregion
 
-      RibbonItem ri1 = AECPanelDebug.AddItem( pushButtonRegisterDockableWindow );
-      RibbonItem ri2 = AECPanelDebug.AddItem( pushButtonShowDockableWindow );
-      RibbonItem ri3 = AECPanelDebug.AddItem( pushButtonHideDockableWindow );
-      #endregion
+            return Result.Succeeded;
+        }
 
-      return Result.Succeeded;
+        public Result OnShutdown(UIControlledApplication a)
+        {
+            return Result.Succeeded;
+        }
     }
 
-    public Result OnShutdown(
-      UIControlledApplication a )
+    public class AvailabilityNoOpenDocument : IExternalCommandAvailability
     {
-      return Result.Succeeded;
+        public bool IsCommandAvailable(UIApplication a, CategorySet b)
+        {
+            if (a.ActiveUIDocument == null)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 
-    private System.Windows.Media.Imaging.BitmapSource GetImage(
-      IntPtr bm )
+    [Transaction(TransactionMode.Manual)]
+    public class RegisterDockableWindow : IExternalCommand
     {
-      System.Windows.Media.Imaging.BitmapSource bmSource
-        = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
-          bm,
-          IntPtr.Zero,
-          System.Windows.Int32Rect.Empty,
-          System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions() );
+        MainPage m_MyDockableWindow = null;
 
-      return bmSource;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            DockablePaneProviderData data
+              = new DockablePaneProviderData();
+
+            MainPage MainDockableWindow = new MainPage();
+            m_MyDockableWindow = MainDockableWindow;
+
+            data.FrameworkElement = MainDockableWindow
+              as System.Windows.FrameworkElement;
+
+            data.InitialState = new DockablePaneState();
+
+            data.InitialState.DockPosition
+              = DockPosition.Tabbed;
+            
+            data.InitialState.TabBehind = DockablePanes
+              .BuiltInDockablePanes.ProjectBrowser;
+
+            DockablePaneId dpid = new DockablePaneId(
+              new Guid("{D7C963CE-B7CA-426A-8D51-6E8254D21157}"));
+
+            commandData.Application.RegisterDockablePane(
+              dpid, "AEC Dockable Window", MainDockableWindow
+              as IDockablePaneProvider);
+
+            return Result.Succeeded;
+        }
     }
-  }
-
-  /// <summary>
-  /// You can only register a dockable dialog in "Zero doc state"
-  /// </summary>
-  public class AvailabilityNoOpenDocument
-    : IExternalCommandAvailability
-  {
-    public bool IsCommandAvailable(
-      UIApplication a,
-      CategorySet b )
-    {
-      if( a.ActiveUIDocument == null )
-      {
-        return true;
-      }
-      return false;
-    }
-  }
-
-  /// <summary>
-  /// Register your dockable dialog
-  /// </summary>
-  [Transaction( TransactionMode.Manual )]
-  public class RegisterDockableWindow
-    : IExternalCommand
-  {
-    MainPage m_MyDockableWindow = null;
-
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      DockablePaneProviderData data
-        = new DockablePaneProviderData();
-
-      MainPage MainDockableWindow = new MainPage();
-
-      m_MyDockableWindow = MainDockableWindow;
-
-      //MainDockableWindow.SetupDockablePane(me);
-
-      data.FrameworkElement = MainDockableWindow
-        as System.Windows.FrameworkElement;
-
-      data.InitialState = new DockablePaneState();
-
-      data.InitialState.DockPosition
-        = DockPosition.Tabbed;
-
-      //DockablePaneId targetPane;
-      //if (m_targetGuid == Guid.Empty)
-      //    targetPane = null;
-      //else targetPane = new DockablePaneId(m_targetGuid);
-      //if (m_position == DockPosition.Tabbed)
-
-      data.InitialState.TabBehind = DockablePanes
-        .BuiltInDockablePanes.ProjectBrowser;
-
-      DockablePaneId dpid = new DockablePaneId(
-        new Guid( "{D7C963CE-B7CA-426A-8D51-6E8254D21157}" ) );
-
-      commandData.Application.RegisterDockablePane(
-        dpid, "AEC Dockable Window", MainDockableWindow
-        as IDockablePaneProvider );
-
-      commandData.Application.ViewActivated
-        += new EventHandler<ViewActivatedEventArgs>(
-          Application_ViewActivated );
-
-      return Result.Succeeded;
-    }
-
-    void Application_ViewActivated(
-      object sender,
-      ViewActivatedEventArgs e )
-    {
-      m_MyDockableWindow.lblProjectName.Content
-        = e.Document.ProjectInformation.Name;
-    }
-  }
-
-  /// <summary>
-  /// Show dockable dialog
-  /// </summary>
-  [Transaction( TransactionMode.ReadOnly )]
-  public class ShowDockableWindow : IExternalCommand
-  {
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      DockablePaneId dpid = new DockablePaneId(
-        new Guid( "{D7C963CE-B7CA-426A-8D51-6E8254D21157}" ) );
-
-      DockablePane dp = commandData.Application
-        .GetDockablePane( dpid );
-
-      dp.Show();
-
-      return Result.Succeeded;
-    }
-  }
-
-  /// <summary>
-  /// Hide dockable dialog
-  /// </summary>
-  [Transaction( TransactionMode.ReadOnly )]
-  public class HideDockableWindow : IExternalCommand
-  {
-    public Result Execute(
-      ExternalCommandData commandData,
-      ref string message,
-      ElementSet elements )
-    {
-      DockablePaneId dpid = new DockablePaneId(
-        new Guid( "{D7C963CE-B7CA-426A-8D51-6E8254D21157}" ) );
-
-      DockablePane dp = commandData.Application
-        .GetDockablePane( dpid );
-
-      dp.Hide();
-      return Result.Succeeded;
-    }
-  }
 }
